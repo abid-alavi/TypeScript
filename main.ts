b002432931ba358a155b7c916270e51cc0d405a4
abid-alavi <abid_alavi88@hotmail.com>
2015-05-22 16:41:46 +0500

    First commit

---
+++ main.ts
@@ -0,0 +1,28 @@
+/*var x = "hello world";
+
+var y = x.charAt(0);
+alert(y);*/
+
+/*var myString : string = "test";
+var myNumber: number = 1;
+var myBoolean : boolean = true;
+
+myString = myNumber;
+myBoolean = myString;
+myNumber = myBoolean;*/
+
+/*var complexType = { name: "myName", id: 1 };
+complexType = { id: 2 };
+complexType = { name: "anotherName" };
+complexType = { address: "address" };*/
+
+var name = "ABID"
+alert(name);
+name = 1;
+alert(name);
+var myFunction = function() {
+	alert(name);
+};
+setTimeout(function() {
+	myFunction
+},1000);
